f848c8a957df4c1ecab19b2e637be92bc9f7d324
Petru12358 <petrubc@yahoo.com>
2015-08-15 18:45:52 +0300

    Release_03, aded changes

--- fndt.py
+++ fndt.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python
 import sys
 import termcolor
+from termcolor import colored
 import colorama
 from colorama import Back, Style
 colorama.init()
+l=[]
+start = "\033[3;5m"
+end = "\033[0;0m"
+def print_Title(myString):
+	print colored ("Welcome and congratulations!The searching string:'" + start + myString + "'"+end + " was founded in lines: ", 'yellow')
+	
 if (len(sys.argv)==5 and sys.argv[1]=='-f'and sys.argv[3]=='-v'):
 
-	def searchString(fileName=sys.argv[2], string=sys.argv[4]):
-		f=fileName
-		myFile = open(f)
-		myString=string
-		l=[]
+	def searchString(fileName=sys.argv[2], myString=sys.argv[4]):
+		myFile = open(fileName)
+		print_Title(myString)
 		n=0
-		print "The string: '" + myString + "' was founded in lines: "
 		for line in myFile:
 			n+=1
         		if myString in line:
 				l.append(n)
-				line=line.replace(myString, termcolor.colored(Back.GREEN+myString+Style.RESET_ALL, 'red'))				
+				line=line.replace(myString, termcolor.colored(Back.GREEN+start+myString+Style.RESET_ALL+end, 'red'))				
     				print l,line 
 				del l[0]
+			
 
 
 
 	searchString()
-if (len(sys.argv)==4 and sys.argv[1]=='-f'):
-	def searchString1(fileName=sys.argv[2], string=sys.argv[3]):
-		f=fileName
-		myFile = open(f)
-		myString=string
-		l=[]
+	del sys.argv
+elif (len(sys.argv)==4 and sys.argv[1]=='-f'):
+	def searchString1(fileName=sys.argv[2], myString=sys.argv[3]):
+		myFile = open(fileName)
+		print_Title(myString)
 		n=0
-		print "The string: '" + myString + "' was founded in lines: "
 		for line in myFile:
         		n+=1
         		if myString in line:
@@ -39,6 +42,36 @@ if (len(sys.argv)==4 and sys.argv[1]=='-f'):
     		print l
 
 	searchString1()
+	del sys.argv
+	
+elif(len(sys.argv)>1 and sys.argv[1]=='-v'):
+	def search2(myString=sys.argv[2]):
+		print_Title(myString)	
+		n=0
+		for line in sys.stdin:
+			n+=1
+			if myString in line:
+				l.append(n)
+				line=line.replace(myString, termcolor.colored(Back.GREEN+start+myString+Style.RESET_ALL+end, 'red'))							
+				print l, line
+				del l[0]
+
+	search2()
+	del sys.argv
+else:
+	def search3(myString=sys.argv[1]):
+		print_Title(myString)	
+		n=0
+		for line in sys.stdin:
+        		n+=1
+        		if myString in line:
+        	    		l.append(n)
+
+    		print l
+
+
+	search3()
+	del sys.argv
 
 
 
